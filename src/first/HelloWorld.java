40ff8f1def2ff99c69dc33a3070b6864b1460dce
techtorial <info@techtorialacademy.com>
2021-11-18 14:17:53 -0600

    Java First commit

---
+++ src/first/HelloWorld.java
@@ -0,0 +1,36 @@
+package first;
+
+public class HelloWorld {
+
+    // this is working like an engine of a car. I need main method to run my code
+    public static void main(String[] args) {
+
+
+        System.out.println("I love Java");
+        System.out.println("I am going to do it"); // this is is just printing
+        // this is single line comment
+        // this is second line
+        /*
+        multiple line
+        for comments
+         */
+
+        System.out.println("Number");
+
+        int number = 5;
+        int number1 = 10;
+        System.out.println(number);
+        System.out.println("Number: "+number);
+        System.out.println("Numbers are: " + number + number1);
+        System.out.println("Numbers are:"+number + " and "+ number1);
+        int number2 = 20;
+        System.out.println(number+number1);
+        System.out.println("Numbers are: " + (number + number1));
+        System.out.println("The sum of my numbers is: " + (number + number1));
+
+
+
+
+
+
+}}
---
+++ src/first/VariablePractice.java
@@ -0,0 +1,57 @@
+package first;
+
+public class VariablePractice {
+
+    public static void main(String[] args){
+
+
+        int number1 = 6;
+        // int number1 = 7; you can not create a variable with same name more than once
+
+        System.out.println(number1);
+
+        number1 = 7;  // reassignment(reassigning the value for variable)
+        System.out.println(number1);
+
+        int $number = 6;
+        int number$ = 7;
+      //  int *number = 9; you can not start naming with *
+        int _number = 9;
+        int num$ber = 10;
+        int number3 = 11;
+
+      //  int class = 12; class is a reserved keyword for java
+
+        //Task: create two int variables and print as :
+        // My numbers are =  <number1> and <number2>
+
+       int myFirstNumber = 666;
+        int mySecondNumber = 222;
+        System.out.println("My numbers are--> "+ myFirstNumber+ " and "+mySecondNumber);
+
+        System.out.println("My first number is: " + myFirstNumber);
+        System.out.println("My first number is: " + myFirstNumber);
+        System.out.println("My first number is: " + myFirstNumber);
+        System.out.println("My first number is: " + myFirstNumber);
+        System.out.println("My first number is: " + myFirstNumber);
+        System.out.println("My first number is: " + myFirstNumber);
+        System.out.println("My first number is: " + myFirstNumber);
+        System.out.println("My first number is: " + myFirstNumber);
+        System.out.println("My first number is: " + 555);
+        System.out.println("My first number is: "+ 555);
+
+        int a, b, c;  // you can declare and initialize variable on the same line
+                        // if they have same data type
+        a=1; b=2; c=4;
+
+        int x =1, y=2, z=3;// you can declare and initialize variable on the same line
+        // if they have same data type
+
+        int dfjagqergbqwfbqjbqjvbr=44;
+        /*
+
+         */
+
+    }
+
+}
