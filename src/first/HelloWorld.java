b24c68312cba71b947455b49fa02ae1c59fa2a65
techtorial <info@techtorialacademy.com>
2022-03-21 19:48:05 -0500

    US_2551 FIRST CODE IMPLEMENTATION

--- src/first/HelloWorld.java
+++ src/first/HelloWorld.java
@@ -27,6 +27,8 @@ public class HelloWorld {
         System.out.println(number+number1);
         System.out.println("Numbers are: " + (number + number1));
         System.out.println("The sum of my numbers is: " + (number + number1));
+        System.out.println("Ion just made a changes");
+        System.out.println("Ion just made a change");
 
 
 
